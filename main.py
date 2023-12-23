7f1f3709c2abfe450a971256d1258317e87a9688
kevinnkimm <43616290+kevinnkimm@users.noreply.github.com>
2023-12-23 14:29:59 -0500

    Adding a json file to load in email address and creating an availbility function to check if an item is available

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
+import json
 import requests
+import smtplib
 from bs4 import BeautifulSoup
+from email.message import EmailMessage
+from datetime import datetime
 
-# checks inventory
+log = ""
 
-def checkInventory(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+def check_availability(url):
+    global log
+    try:
+        page = requests.urlopen(url)
+        soup = BeautifulSoup(page, features='html.parser')
+
+        if phrase in soup.text:
+            return False
+        return True
+    except:
+        log += "Error parsing the website"
 
 
-# indicate a specific class using inspect element of where the in stock/out of stock could be located
-    inventory_status = soup.find(class_='a-size-medium a-color-success').get_text()
+def main():
+    url = "https://www.bestbuy.ca/en-ca/product/playstation-5-slim-console-marvel-s-spider-man-2-bundle/17390652"
+    phrase = "PlayStation 5"
+    available = check_availability(url, phrase)
 
-    if "Add to Cart" in inventory_status:
-            print("Item is in stock!")
-    
-    else:
-            print("Item is out of stock.")
+    if available:
 
-url = 'https://www.bestbuy.ca/en-ca/product/hp-15-6-laptop-natural-silver-intel-core-i5-1235u-512gb-ssd-16gb-ram-windows-11-home/17210546'
-checkInventory(url)
+        with open('config.json') as file:
+            config = json.load(file)
+            username = config['username']
+            password = config['password']
+            fromAddress = config['fromAddress']
+            toAddress = config['toAddress']
+
+        msg = EmailMessage()
+        msg['Subject'] = "Playstation 5 is in stock!"
+        msg['From'] = config['fromAddress']
+        msg['To'] = config['toAddress']
+        msg.set_content("It seems that there is a PS5 available at " + url)
+
+        server = smtplub.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.Login(username,password)
+
+        server.send_message(msg)
+        server.quit()
+
+        
+
+if __name__ == '__main__':
+    main()
